81021da67dc22b26870ffd4638fcf1c78e5bb054
Shaddy <szayour@gmail.com>
2015-11-18 07:41:59 -0600

    Starting to be able to submit recipes
    recipe submission prototype

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,21 @@
 class RecipesController < ApplicationController
+	def show
+		@recipe = Recipe.find(params[:id])
+	end
+
 	def new
 	end
 
 	def create
-		render plain: params[:recipe].inspect
+		@recipe = Recipe.new(params.require(:recipe).permit(:title, :category, :preptime, :ingredients, :directions, :calories, :fats, :carbs, :protein))
+ 
+  		@recipe.save
+  		redirect_to @recipe
 	end
+
+	private
+  		def recipe_params
+    		params.require(:recipe).permit(:title, :category, :preptime, :ingredients, :directions, :calories, :fats, :carbs, :protein)
+  		end
 
 end
